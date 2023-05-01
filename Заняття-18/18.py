bb2f863f3e399b64fbeab6f6dcbc70006bf5def6
bobantonbob <antonbabenko1983@gmail.com>
2023-05-01 22:28:25 +0300

    Заняття18

---
+++ Заняття-18/18.py
@@ -0,0 +1,11 @@
+import time
+
+def speed_test(function):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        end_time = time.time()
+        print(f"Час виконання {function.__name__}: {end_time - start_time} секунд")
+        return result
+    return wrapper
+
